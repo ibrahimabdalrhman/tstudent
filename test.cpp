7a6d43f5b740b02e21d294e1d2c0cad83edfc237
Ibrahim Abdalrhman <59165733+ibrahimabdalrhman@users.noreply.github.com>
2023-01-04 10:41:37 +0200

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int num1,num2;
+	char op;
+	cin>>num1>>op>>num2;
+	if(op=='*'){
+		cout<<num1*num2;
+	}
+	else if(op=='/'){
+		cout<<num1/num2;
+	}
+	else if(op=='+'){
+		cout<<num1+num2;
+	}
+	else if(op=='-'){
+		cout<<num1-num2;
+	}
+	
+	
+	return 0 ;
+}
